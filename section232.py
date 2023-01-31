d5dfae539904ea1bc318deaff12352938d4154d5
Harry NH Cho <mr.cnh9110@gmail.com>
2023-01-30 22:52:37 -0800

    loop through the rows and append the data to the dictionary

--- section232.py
+++ section232.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
+from datetime import datetime
 
 
 options = webdriver.ChromeOptions()
@@ -21,7 +22,7 @@ time.sleep(2)
 show_entries = driver.find_element(By.NAME, "erdashboard_length")
 show_entries.send_keys("100")
 time.sleep(2) # waits for the page to reload when the filter is updated to show 100 entries
-page_source = driver.page_source # gets the page with 100 entries, which we will use in BeuatifulSoup
+page_source = driver.page_source # gets the page source with 100 entries, which we will use in BeuatifulSoup
 
 soup = BeautifulSoup(page_source, "lxml")
 
@@ -32,10 +33,34 @@ for each in th:
     column_names.append(each.text.strip())
 print(column_names)
 
+# creates an empty dictionry with keys from the extracted column names
+exclusion_request_dict = dict.fromkeys(column_names)
 
+# initialize exclusion_request_dict with each column to be an empty list
+for each in column_names:
+    exclusion_request_dict[each] = []
+
+print(exclusion_request_dict)
+
+# finds all rows in the table
 tr = soup.find_all("tbody")[0].find_all("tr")
 
-# count = 0
-# for record in tr:
-#     count += 1
-#     print(count, record)
+# loops through the rows and append the data to the dictionary
+count = 0
+for record in tr:
+    count += 1
+    tr_data = record.find_all("td")
+    print(count, tr_data)
+
+    date = datetime.strptime(tr_data[6].string, "%m/%d/%Y").date()
+
+    exclusion_request_dict["ID"].append(int(tr_data[0].string))
+    exclusion_request_dict["Company"].append(tr_data[1].string)
+    exclusion_request_dict["Product"].append(tr_data[2].string)
+    exclusion_request_dict["HTSUSCode"].append(tr_data[3].string)
+    exclusion_request_dict["Status"].append(tr_data[4].string)
+    exclusion_request_dict["Days Remaining"].append(int(tr_data[5].string))
+    exclusion_request_dict["Posted Date"].append(date)
+    exclusion_request_dict["Details"].append(tr_data[7].string)
+
+
